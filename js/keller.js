3f0656774ae778893419177a3dc29abe920699ed
keller <kellerlong@163.com>
2016-10-15 16:47:55 +0800

    creaste detailed

--- js/keller.js
+++ js/keller.js
@@ -14,20 +14,45 @@ var FM_CONFIG                   = {
     _private.init               = function ( data ) {
         this._config            = data;
     };
-    $public.loadMedia           = function () {
+    $public.loadMedia           = function (event) {
         var config              = this._config;
         var audio               = this.audio;
         if( !audio ){
             audio               = this.audio = document.createElement('audio');
         }
         audio.src               = config.fmURL;
+        for( var key in event ){
+            audio[key]          = event[key];
+        }
 
 
     };
-    window.$fmPlay              = FMPlay( FM_CONFIG );
+    $public.play                = function () {
+        this.audio.play();
+    };
+    $public.getAllTime          = function () {
+        return this.audio.duration;
+    };
+    window.$fmPlay              = new FMPlay( FM_CONFIG );
+
 })(window, FM_CONFIG);
 
-document.addEventListener('DOMContentLoaded', function (e) {
-
-    $fmPlay.loadMedia();
-}, false);
+window.$View                    = {
+    detailedInit: function () {
+        $fmPlay.loadMedia({
+            ontimeupdate: function ( e ) {
+                $View.detailedSetPlan( e.path[0].currentTime );
+            }
+        });
+        $fmPlay.play();
+    },
+    detailedSetPlan: function (time) {
+        var allTime             = $fmPlay.getAllTime();
+        var planLength          = time / allTime * 100;
+        var planDom             = document.getElementById('detailed-plan-full');
+        planDom.style.width     = planLength + "%";
+        var playBtn             = document.getElementById('detailed-content-play');
+        playBtn.innerHTML     = '&#xe62f;';
+    }
+};
+document.addEventListener('DOMContentLoaded', $View.detailedInit, false);
